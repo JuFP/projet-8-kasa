09a5371a7d7cbf646d14c2dfed1ee4730b037f21
Justine Faria Pereira <justine.fariapereira@yahoo.com>
2023-05-18 18:42:33 +0200

    ajout de react router

---
+++ src/pages/error-404.jsx
@@ -0,0 +1,8 @@
+const Error = () => {
+    return (
+        <div className="error">
+            Page erreur 404
+        </div>
+    );
+}
+export default Error;
---
+++ src/pages/home.jsx
@@ -0,0 +1,8 @@
+const Home = () => {
+    return (
+        <div className="home">
+            Page d'accueil
+        </div>
+    );
+}
+export default Home;
